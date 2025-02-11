b2cc39a4fc8f60413a184be3ff3840cbf532a098
redcommander561 <jaysona803@deltalearns.ca>
2025-02-11 11:27:20 -0800

    .
    .

--- task1.py
+++ task1.py
@@ -19,3 +19,46 @@ Fred Jones      fred@city.com   6045553434      102     75 57 Street            
 Leroy Jenkins   leroy@wow.ca    2342222323      103     65 Blizzard Ave         100     
 Jen Mezei       jen@shaw.ca     6042231134      104     891 Cullen Cresc        0
 """
+
+import sqlite3
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+print(connection)
+
+cursor = connection.cursor()
+cursor.execute("drop table if exists customers")
+
+
+query = """
+create table if not exists customers (
+id integer primary key autoincrement,
+name text,
+email text,
+phoneNum text,
+address text,
+balance integer
+);
+"""
+cursor.execute(query)
+
+data = [
+    ['Joe Smith', 'joe@gmail.com', '7783341111',101,'1234 Sesame Street', 0],
+    ['Fred Jones', 'fred@city.com', '6045553434',102, '75 57 Street', 0],
+    ['Leroy Jenkins', 'leroy@wow.ca', '2342222323',103, '65 Blizzard Ave', 100],
+    ['Jen Mezei', 'jen@shaw.ca', '6042231134',104, '891 Cullen Cresc', 0]
+]
+
+for i in data:
+    query = f"insert into customers (name, email, phoneNum, id , address, balance) values ('{i[0]}', '{i[1]}', '{i[2]}', {i[3]}, '{i[4]}', {i[5]});"
+    cursor.execute(query)
+
+connection.commit()
+
+query = "select * from customers"
+cursor.execute(query)
+result = cursor.fetchall()
+for i in result:
+    print(i)
+
+connection.close()
--- task2.py
+++ task2.py
@@ -21,3 +21,49 @@ Casey           cat             Siberian        8    m          true            
 Friend          cat             Domestic        4    m          false               102
 Copper          dog             Beagle          12   m          true                104
 """
+
+
+import sqlite3
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+print(connection)
+
+cursor = connection.cursor()
+cursor.execute("drop table if exists customers")
+
+query = """
+create table if not exists customers (
+id integer primary key autoincrement,
+name text,
+species text,
+breed text,
+age text,
+gender text,
+spayed text,
+ownerID integer
+);
+"""
+cursor.execute(query)
+
+data = [
+    ['Fluffy', 'dog', 'Pomeranian','5','M','true', 101],
+    ['Benjamin', 'cat', 'Siberian','8','M','true', 103],
+    ['Casey', 'cat', 'Siberian','8','M','true', 103],
+    ['Friend', 'cat', 'Domestic','4','M','false', 102],
+    ['Copper', 'dog', 'Beagle','12','M','true', 104],
+]
+
+for i in data:
+    query = f"insert into customers (name, species,breed, age, gender, spayed, ownerID) values ('{i[0]}', '{i[1]}', '{i[2]}', '{i[3]}', '{i[4]}', '{i[5]}',{i[6]});"
+    cursor.execute(query)
+
+connection.commit()
+
+query = "select * from customers"
+cursor.execute(query)
+result = cursor.fetchall()
+for i in result:
+    print(i)
+
+connection.close()
